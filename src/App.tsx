a84dfabdb61c98850f2f0c0d58f6d5a0d7007795
ichigo-dev <ichigo@web-freework.com>
2021-07-13 15:42:26 +0900

    add: logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+//axios.defaults.withCredentials = true;
 
 function App() {
+  const clickHandle = () => {
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+    };
+    const config = {
+      headers: headers,
+      withCredentials: true
+    };
+
+    axios.get('http://localhost:3000', config).then((res) => {
+        console.log(res);
+      }).catch((err) => {
+          console.log(err);
+      });
+  };
+
+  const clickLogoutHandle = () => {
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+    };
+    const config = {
+      headers: headers,
+      withCredentials: true
+    };
+
+    axios.delete('http://localhost:3000/auth/sign_out', config).then((res) => {
+        console.log(res);
+      }).catch((err) => {
+          console.log(err);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,12 +47,14 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:3000/auth/google"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <button onClick={ () => clickHandle() }>リクエスト</button>
+        <button onClick={ () => clickLogoutHandle() }>ログアウト</button>
       </header>
     </div>
   );
